cd73b776027bd8035f32203c950cbb8ba13278c0
willpotato <60055879+willpotato@users.noreply.github.com>
2020-12-11 14:02:57 -0500

    Update index.html

--- docs/assets/js.js
+++ docs/assets/js.js
@@ -1,6 +1,15 @@
 // scroll-to-top button
 function ttop() {
-        var body = $("html, body");
-        body.stop().animate({scrollTop:0}, 800, 'swing', function() {
-        });
-    }
+    var body = $("html, body");
+    body.stop().animate({scrollTop:0}, 800, 'swing', function() {
+    });
+}
+
+function copyIP() {
+	var iptxt = document.getElementById("ip");
+	var ip = iptxt.substr(6, 8);
+	ip.select();
+	ip.setSelectionRange(0, 99999);
+	document.execCommand("copy");
+	console.log(ip)
+}
