1dfc966474d271f2858f3ed27260baac3f8da647
willpotato <60055879+willpotato@users.noreply.github.com>
2022-02-17 18:03:01 -0500

    june 2021

--- docs/assets/js.js
+++ docs/assets/js.js
@@ -6,18 +6,16 @@ function ttop() {
 }
 
 //ip select on click
+
 function copyIP() {
-	var iptxt = document.getElementById("ip");
-	var ip = iptxt.substring(6, 11);
-	ip.select();
-	ip.setSelectionRange(0, 99999);
-	document.execCommand("copy");
-	console.log(ip)
+	var ip = document.getElementById("ip").innerHTML;
+	var iptxt = ip.substring(6, 16);
+  navigator.clipboard.writeText(iptxt);
 }
 
 //service worker init
 
-if('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker
   .register('./assets/sw.js')
   .then(function() {
@@ -29,16 +27,27 @@ if('serviceWorker' in navigator) {
 }
 
 //get srvdata
-$.getJSON('https://eu.mc-api.net/v3/server/ping/hypixel.net',
-  function(data) {
-    $.each(data, function(index, value) {
-      // console.log(value);
-    });
-    var online = data.version.name;
-    var status = data.online
-    var playermax = data.players.max
-    var players = data.players.online
-    console.log(online);
+$.getJSON('https://eu.mc-api.net/v3/server/ping/cheems.xyz', function(srvdata) {
+  console.log("Server online: " + srvdata.online)
+
+  /*
+  if (srvdata.online = "true") {
+    console.log("Server ping successful")
+    var version = srvdata.version.name
+    var status = srvdata.online
+    var playermax = srvdata.players.max
+    var players = srvdata.players.online
+    console.log(version);
     console.log(" " + status + " ")
     console.log(players + "/" + playermax)
-  });
+  } else {
+    if (srvdata.online = "false") {
+      console.log("Server ping failed")
+      console.log(srvdata.online)
+    } else {
+      console.log("Server ping error")
+      console.log(srvdata.online)
+    };
+  };
+  */
+});
